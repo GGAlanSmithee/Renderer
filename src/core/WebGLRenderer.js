ddd77877d8ae793d6645a918c1ddcef021e701f2
Alan Smithee <ggnore.alan.smithee@gmail.com>
2015-06-04 18:38:44 +0000

    adds WebGLUtils class
    the utils class is responsible for accessing WebGL functionality that does not
    belong in any other class. such as getting a context, creating buffers etc

--- src/core/WebGLRenderer.js
+++ src/core/WebGLRenderer.js
@@ -1,22 +1,13 @@
 Renderer.WebGLRenderer = function(gl, textures) {
 };
 
-Renderer.WebGLRenderer.SpritesPerDrawCall = Math.floor(65000/6);
+Renderer.WebGLRenderer.MaxNumbSprites = Math.floor(65000/6);
 
 Renderer.WebGLRenderer.prototype = {
     constructor : Renderer.WebGLRenderer,
     
     init : function(gl, vs, fs) {
         
-    },
-    
-    createVertexBufferData : function() {
-    },
-    
-    createVertexIndexData : function() {
-    },
-
-    addTexture : function(gl, name, path) {
     },
     
     clear : function(gl) {
